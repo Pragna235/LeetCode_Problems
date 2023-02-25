39e242711c47eb94ec5cd5592b29cc78f25fe6bf
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-02-25 12:27:25 +0530

    Add files via upload
    25th February 2023

---
+++ 121_BestTimeToBuynSellStock.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& arr) {
+    int maxPro = 0;
+    int n = arr.size();
+    int minPrice = INT_MAX;
+
+    for (int i = 0; i < arr.size(); i++) {
+        minPrice = min(minPrice, arr[i]);
+        maxPro = max(maxPro, arr[i] - minPrice);
+    }
+
+    return maxPro;
+    }
+};
