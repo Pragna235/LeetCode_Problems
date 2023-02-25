281b4ee21c1b7b61548c01387e1afc51132ea3e3
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-02-25 11:51:24 +0530

    Add files via upload
    25th February 2023

---
+++ 53_Maximum_Subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum=0;
+        int maxi = INT_MIN;
+        for(auto it : nums)
+        {
+            sum+=it;
+            maxi=max(sum,maxi);
+            if(sum<0)
+            sum=0;
+
+        }
+        return maxi;
+
+
+    }
+};
