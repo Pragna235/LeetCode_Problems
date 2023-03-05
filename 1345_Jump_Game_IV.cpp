71ecd22af4c9765556dbfb8a5276baa0c4865c1f
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-03-05 22:51:27 +0530

    Add files via upload
    5th March 2023

---
+++ 1345_Jump_Game_IV.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int minJumps(vector<int>& arr)
+    {
+        int n = arr.size();
+        unordered_map<int, vector<int>>mp;
+        for (int i = 0; i < n; i++) mp[arr[i]].push_back(i);
+
+        queue<int>q;
+        vector<bool>visited(n, false);
+        q.push(0);
+        int steps = 0;
+        while(!q.empty())
+        {
+            int size = q.size();
+            while(size--)
+            {
+                int currIdx = q.front();
+                q.pop();
+                if (currIdx == n - 1) return steps;
+                //================================================================
+                //EXPLORE ALL POSSIBLE OPTIONS
+                if (currIdx + 1 < n && !visited[currIdx + 1])  //OPTION-1 (Move Forward)
+                {
+                    visited[currIdx + 1] = true;
+                    q.push(currIdx + 1);
+                }
+                if (currIdx - 1 >= 0 && !visited[currIdx - 1]) //OPTION-2 (Move Backward)
+                {
+                    visited[currIdx - 1] = true;
+                    q.push(currIdx - 1);
+                }
+                for (int newIdx : mp[arr[currIdx]])  //OPTION-3 (Move to same valued idx)
+                {                                 //newIdx coud be before currIdx or after currIdx
+                    if (!visited[newIdx])
+                    {
+                        visited[newIdx] = true;
+                        q.push(newIdx);
+                    }
+                }
+                //===================================================================
+                mp[arr[currIdx]].clear();    //EXPLAINED BELOW :)
+            }
+            steps++;
+        }
+        return -1;
+    }
+};
