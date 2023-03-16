3e636ebc31340e458438588589eac8e2473b5537
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-03-16 21:29:34 +0530

    Add files via upload
    16th March 2023

---
+++ 106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& ino, vector<int>& post) {
+    int i1 = post.size()-1;
+        return solve(i1,ino,post,0,ino.size()-1);
+    }
+    TreeNode* solve(int &i,vector<int> &in,vector<int> &post,int l,int r){
+        if(l>r)return NULL;
+        int x = r;
+        while(post[i] != in[x]){
+            x--;
+        }
+        i--;
+        TreeNode* root = new TreeNode(in[x]);
+        root->right = solve(i,in,post,x+1,r);
+        root->left = solve(i,in,post,l,x-1);
+        return root;
+    }
+};
