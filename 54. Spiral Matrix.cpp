2d4411e239bac8d4075054c42886382de8f591f0
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-07-11 20:41:27 +0530

    Create 54. Spiral Matrix.cpp

---
+++ 54. Spiral Matrix.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> ans ;
+        int row=matrix.size();
+        int col = matrix[0].size();
+        int top=0,left=0,bottom=row-1,right=col-1;
+        while(left<=right && top<=bottom){
+            for(int i=left;i<=right;i++){
+                ans.push_back(matrix[top][i]);
+            }
+            top++;
+
+            for(int i=top;i<=bottom;i++){
+                ans.push_back(matrix[i][right]);
+            }
+            right--;
+
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
+                    ans.push_back(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
+                    ans.push_back(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans;
+        
+    }
+};
