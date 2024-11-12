9e387f803148a47315fee9da619a7d7d967ae6c5
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-11-12 11:22:55 +0530

    Create 3133. Minimum Array End.cpp

---
+++ 3133. Minimum Array End.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long minEnd(int n, int x) {
+        long result = x;
+        long remaining = n - 1;
+        long position = 1;
+        
+        while (remaining) {
+            if (!(x & position)) {
+                result |= (remaining & 1) * position;
+                remaining >>= 1;
+            }
+            position <<= 1;
+        }
+        
+        return result;
+    }
+};
